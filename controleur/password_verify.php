f3868b3b0965f51bc9a0996d7fe26f058ff1713e
Unknown <laurianepierre@outlook.fr>
2017-01-23 15:31:26 +0100

    Connexion à la DB password hash et verify et register

---
+++ controleur/password_verify.php
@@ -0,0 +1,5 @@
+<?php
+    $password = "RECUPERATION MDP"
+    $hashed_password = "$2y$10$BBCpJxgPa8K.iw9ZporxzuW2Lt478RPUV/JFvKRHKzJhIwGhd1tpa";
+    password_verify($password, $hashed_password);
+?>
